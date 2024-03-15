db88f739cb845b8fa3a74089ef1c3939c289a27f
azziimm7 <azzah31@outlook.com>
2024-03-15 17:15:17 +0200

    Update User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This module defines a class User
+    inherits from basemode and
+    Base
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalachemy.ext.declarative import declarative_base
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by
+    attributes:
+    email,
+    password,
+    first_name and last_name
+    """
+
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128)
+    last_name = Column(String(128), nullable)
