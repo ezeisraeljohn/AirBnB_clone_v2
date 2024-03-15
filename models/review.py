9c67ce85c7ee0e7e3460634db0dad3177c64308d
azziimm7 <azzah31@outlook.com>
2024-03-15 19:10:38 +0200

    update place

--- models/review.py
+++ models/review.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""This script contains review attributes """
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
 
 class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    """ Review class inherits from basemodel and base
+    to store review information
+    Attributes:
+    place_id
+    user_id
+    text
+
+    """
+
+    __tablename__ = "reviews"
+    place_id = Column(String(60), nullable=False, ForignKey("place.id")
+    user_id = Column(String(60), nullable=False, ForignKey("user.id")
+    text = Column(String(1024), nullable=False)
--- models/user.py
+++ models/user.py
@@ -4,7 +4,8 @@
     Base
 """
 from models.base_model import BaseModel, Base
-from models.place import place
+from models.place import Place
+from models.review import Review
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -22,6 +23,13 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
+<<<<<<< HEAD
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
     places = relationship("Place", backref="user")
+=======
+    first_name = Column(String(128)
+    last_name = Column(String(128), nullable)
+    places = relationship("Place", cascade='all, delete' backref="user")
+    review = relationship("Review", cascade='all, delete' backref="user")
+>>>>>>> update place
