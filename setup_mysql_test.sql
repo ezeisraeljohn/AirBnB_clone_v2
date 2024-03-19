a1a2a621faea25cd6ea41b6ab800f19aa61ef78b
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-19 02:10:26 +0100

    Add MySQL test setup script

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- prepares a MySQL server for the project
+-- creates a test database for this project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
