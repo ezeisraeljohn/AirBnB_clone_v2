53b785747a0585c3cac760a44ac2e624226a75df
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-19 18:53:58 +0100

    Refactor all method in FileStorage to support filtering by class name

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            temp = {}
+            for key, val in self.__objects.items():
+                if cls.__name__ in key:
+                    temp[key] = val
+            return temp
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
