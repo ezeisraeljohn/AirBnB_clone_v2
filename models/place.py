8159250a8a43ae706204b6da9cacbf3dd92c75c5
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-20 20:04:02 +0100

    Remove duplicate import statements in place.py

--- models/place.py
+++ models/place.py
@@ -2,10 +2,8 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.orm import relationship
 
 class Place(BaseModel, Base):
     """ A place to stay
