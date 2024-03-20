9274652923c079965e732bea1cddf83991b14e2a
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-20 15:19:11 +0100

    Refactor User model to include SQLAlchemy mappings for email, password, first_name, last_name, and places

--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This module defines a class User
+    inherits from basemode and
+    Base
+"""
+from models.base_model import BaseModel, Base
+from models.place import place
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by
+    attributes:
+    email,
+    password,
+    first_name and last_name
+    """
+
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user")
