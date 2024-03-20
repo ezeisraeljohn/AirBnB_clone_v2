ef9a40a8fe391483acbda25d0943e4c4435dae94
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-20 15:20:18 +0100

    Refactor City model and add places relationship

--- models/city.py
+++ models/city.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from models.place import Place
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import declarative_base
 
-
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
+
+
     __tablename__ = "cities"
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = ""
+    name = ""
+
+    places = relationship("Place", cascade='all, delete', backref="cities")
