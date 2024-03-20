631dd4f8a6e58b28d86924872985e04d7b2d3fbe
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-20 08:14:31 +0100

    Add imports for new model classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -34,14 +41,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -61,5 +60,5 @@ class FileStorage:
             if obj:
                 key = obj.__class__.__name__ + '.' + obj.id
                 if key in self.__objects:
-                         del self.__objects[key]
+                    del self.__objects[key]
                 self.save()
