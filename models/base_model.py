edc80453625f159154359e7a74af23de393720ea
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-23 22:37:30 +0100

    Refactor BaseModel initialization and add missing imports

--- models/base_model.py
+++ models/base_model.py
@@ -14,20 +14,31 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            self.id = Column(String(60), primary_key=True, nullable=False)
-            self.created_at = Column(DateTime, default=datetime.utcnow())
-            self.updated_at = Column(DateTime, default=datetime.utcnow())
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            
+            if 'updated_at' not in kwargs:
+                self.created_at = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime.fromisoformat(kwargs
+                                                              ['updated_at'])
+            if 'created_at' not in kwargs:
+                self.updated_at = datetime.now()
+            else:
+                kwargs['created_at'] = datetime.fromisoformat(kwargs
+                                                              ['created_at'])
 
             for key, value in kwargs.items():
-                if key != '__class__':
+                if key != "__class__":
                     setattr(self, key, value)
+            if "__class__" in self.__dict__:
+                del self.__dict__["__class__"]
+        models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
