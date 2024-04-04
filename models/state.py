af3cfecc61ebc4c0f3db525f9e4e7e574a3807af
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-04 20:17:26 +0100

    Add primary key to State model and update cities relationship

--- models/state.py
+++ models/state.py
@@ -10,5 +10,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
 
+    id = Column(String(60), primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    # cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state")
