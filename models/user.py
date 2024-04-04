2236135471776bcda36ac712f1655de3cf1dd283
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-04 20:18:34 +0100

    Add id column to User model

--- models/user.py
+++ models/user.py
@@ -20,7 +20,7 @@ class User(BaseModel, Base):
     """
 
     __tablename__ = "users"
-
+    id = Column(String(60), primary_key=True)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
