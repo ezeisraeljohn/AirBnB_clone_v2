1a17e2ae56ba68a9a1a776279be0897fd77729e8
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-04 20:28:04 +0100

    Add setup_web_static.sh script to install nginx, create necessary directories, and update nginx configuration

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Install nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+
+# Create a fake HTML file for testing
+echo "<html><head></head><body>Test Page</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create or recreate symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of /data/ folder to ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update nginx configuration
+config_content="server {
+    listen 80;
+    listen [::]:80 default_server;
+
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html;
+    }
+}"
+
+echo "$config_content" | sudo tee /etc/nginx/sites-available/default
+
+# Restart nginx
+sudo service nginx restart
