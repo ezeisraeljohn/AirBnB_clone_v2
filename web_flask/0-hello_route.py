751c684116c2a2057da7bfdb8f66a605877d2ed6
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-19 23:23:32 +0100

    Add Flask app with hello route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" This module demonstrate how to start a flask app"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+        """ This function runs an app that prints Hello HBNB!"""
+        return "Hello HBNB!"
+
+if __name__=="__main__":
+        app.run(host='0.0.0.0', port=5000)
