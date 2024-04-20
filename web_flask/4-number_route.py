1bf6d90b50f09a61d0401405606eaca839ff4ebf
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-20 14:01:17 +0100

    Changes made

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+""" Module that starts a flask application """
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def welcome_hbnb():
+    """ Function that returns HBNB"""
+    return "Hello HBHB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function that returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    """ Function that returns custom text"""
+    text_str = str(text)
+
+    if '_' in text_str:
+        text_str = text_str.replace('_', ' ')
+    return f"C {text_str}"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """ Function that returns a custom text """
+    text = str(text)
+
+    if '_' in text:
+        text = text.replace('_', ' ')
+
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ Prints out the value of n followed by 'is a number' """
+
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
