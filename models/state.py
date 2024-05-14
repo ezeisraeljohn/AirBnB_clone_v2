0f5bbf021040f935ca3994596e5ef8221328578e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-14 12:09:39 +0100

    refactor: Add method to retrieve list of cities for a state

--- models/state.py
+++ models/state.py
@@ -2,8 +2,12 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
+from models.city import City
+import models
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
 
 
 class State(BaseModel, Base):
@@ -14,3 +18,14 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     # cities = relationship("City", backref="state", cascade="all, delete")
     cities = relationship("City", backref="states")
+
+    def cities(self):
+        """ Return list of city objecs"""
+        if os.getenv("HBNB_TYPE_STORAGE") == "fs":
+            all_cities = models.storage.all(City)
+            list_cities = []
+            for value in all_cities.values():
+                if value.state_id == self.id:
+                    list_cities.append(value)
+            print (list_cities)
+            return list_cities
