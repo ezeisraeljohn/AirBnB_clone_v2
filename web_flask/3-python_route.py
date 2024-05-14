27ec724af4d6ab547cb149b61307374895d7db17
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-14 11:51:39 +0100

    refactor: update Flask app to use index() instead of hbnb()

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,46 +1,35 @@
 #!/usr/bin/python3
-
-""" Module that starts a flask application """
-
+"""
+starts a Flask web application
+"""
 
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def welcome_hbnb():
-    """ Function that returns HBNB"""
-    return "Hello HBHB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Function that returns HBNB"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def C_is_fun(text):
-    text_str = str(text)
-
-    if '_' in text_str:
-        text_str = text_str.replace('_', ' ')
-    return f"C {text_str}"
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
-    text = str(text)
-
-    if '_' in text:
-        text = text.replace('_', ' ')
-
-    return f"Python {text}"
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
