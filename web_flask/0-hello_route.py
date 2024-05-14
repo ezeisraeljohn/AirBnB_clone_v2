7c7d2f55dedc83788105ea5cc6dbccde1f8f3c87
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-14 11:48:18 +0100

    refactor: update Flask app to use index() instead of hbnb()

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-
-""" This module demonstrate how to start a flask app"""
-
+"""
+starts a Flask web application
+"""
 
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb():
-    """ This function runs an app that prints Hello HBNB! """
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
